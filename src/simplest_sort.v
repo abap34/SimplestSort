44b3a159d572637ee221ea420b4b3dd516f90e2c
abap34 <abap0002@gmail.com>
2024-11-27 14:08:30 +0900

    `swap` を再帰なしのものに書き換え

--- src/simplest_sort.v
+++ src/simplest_sort.v
@@ -7,29 +7,36 @@ Require Import List.
 Import ListNotations.
 Require Import Arith.
 
+
 (* --- SimpleSort 自体の実装 --- *)
-
 (* まずスワップ *)
-Fixpoint swap (l : list nat) (i j : nat) : list nat :=
-  match l with
-  | [] => []         
-  (* 空リストの場合は何もしない *)
-  | x :: xs =>       
-  (* head, tail に分ける *)
-    match i, j with  
-    | 0, 0 => x :: xs 
-     (* i = j = 0 の場合はそのままくっつける *)
-    | 0, S j' => nth j' xs x :: firstn j' xs ++ x :: skipn (S j') xs  
-    (* i = 0, j = S j' の場合は [j' 番目, 0 ~ j' 番目, x, j' + 1 ~ 最後] *)
-    | S i', 0 => nth i' xs x :: firstn i' xs ++ x :: skipn (S i') xs
-    (* i = S i', j = 0 の場合は [i' 番目, 0 ~ i' 番目, x, i' + 1 ~ 最後]*)
-    | S i', S j' => x :: swap xs i' j'
-    (* それ以外の場合, head はそのままで、 tail のインデックスを一つ減らして再帰 *)
-    end
-  end.
+Definition swap (l : list nat) (i j : nat) : list nat :=
+  if i =? j then
+    l
+  else
+    if i <? j then
+      firstn i l ++ [nth j l 0] ++ skipn (S i) (firstn j l) ++ [nth i l 0] ++ skipn (S j) l
+    else
+      firstn j l ++ [nth i l 0] ++ skipn (S j) (firstn i l) ++ [nth j l 0] ++ skipn (S i) l.
 
-Compute swap [1; 2; 3; 4; 5] 1 2.
+
+Compute swap [1; 2; 3; 4; 5] 1 2. 
+(* 1; 3; 2; 4; 5 *)
+
+Compute swap [1; 2; 3; 4; 5] 2 1.
+(* 1; 3; 2; 4; 5 *)
+
 Compute swap [1; 2; 3; 4; 5] 0 4.
+(* 5; 2; 3; 4; 1 *)
+Compute swap [1; 2; 3; 4; 5] 4 0.
+(* 5; 2; 3; 4; 1 *)
+
+Compute swap [1; 2; 3; 4; 5] 0 0.
+(* 1; 2; 3; 4; 5 *)
+
+Compute swap [1; 2; 3; 4; 5] 2 2.
+(* 1; 2; 3; 4; 5 *)
+
 
 
 Fixpoint inner_loop (arr : list nat) (n i m : nat) : list nat :=
